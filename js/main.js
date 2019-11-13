93ce38e3d2ff107bf5ac3413f3ff821e6b6f0c08
DJ5007 <you@example.com>
2019-11-13 15:20:22 +0200

    The site have done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+    const responsive = {
+        0: {
+            items: 1
+        },
+        320: {
+            items: 1
+        },
+        560: {
+            items: 2
+        },
+        960: {
+            items: 3
+        }
+    };
+
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
 
@@ -15,8 +30,18 @@ $(document).ready(function () {
         autoplayTimeout: 3000,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
 
+    /* click to scroll top*/
+    $('.move-up span').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1500);
+    });
+
+    /* animation on scroll AOS*/
+    AOS.init();
 
 });
